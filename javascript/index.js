eadcd92a50c97356c9a375901aaed880222e0906
Satishk210 <k.satish151@gmail.com>
2024-02-16 11:26:17 +0530

    updated

---
+++ javascript/index.js
@@ -0,0 +1,54 @@
+//Redclare possible
+var number=3;
+var number1=4;
+console.log(number+number1);
+
+
+////Redclare not possible
+let place="chennai";
+
+//variable reassign possible
+ place="trichy";
+console.log(place);
+
+
+//Redclare not possible
+//variable reassign possible
+const num=3;
+console.log(num);
+
+//data types: String,number,boolean,null,undefined,symbols,object:
+
+//functions
+
+var milk= 23;
+var biscuit =20;
+var cofeePowder= 50;
+
+calculate(milk,biscuit,cofeePowder)
+
+function calculate(a,b,c){
+    var result = a+b+c;
+return result;
+}
+
+
+var bread= 20;
+var bun =25;
+var milkPowder= 10;
+console.log(calculate(milk,biscuit,cofeePowder));
+console.log(calculate(bread,bun,milkPowder));
+
+//for loop
+for(person=0;person<100;person++){
+console.log("go in:"+person);
+}
+
+
+
+
+
+
+
+
+
