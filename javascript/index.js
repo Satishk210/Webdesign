c38d29655e6b8dcf2597bf1884a203f8c568b1c2
Satishk210 <k.satish151@gmail.com>
2024-02-18 16:42:54 +0530

    updated

--- javascript/index.js
+++ javascript/index.js
@@ -1,51 +1,84 @@
-//Redclare possible
-var number=3;
-var number1=4;
-console.log(number+number1);
+// //Redclare possible
+// var number=3;
+// var number1=4;
+// console.log(number+number1);
 
 
-////Redclare not possible
-let place="chennai";
+// Redclare not possible
+// let place="chennai";
 
-//variable reassign possible
- place="trichy";
-console.log(place);
-
-
-//Redclare not possible
-//variable reassign possible
-const num=3;
-console.log(num);
-
-//data types: String,number,boolean,null,undefined,symbols,object:
-
-//functions
-
-var milk= 23;
-var biscuit =20;
-var cofeePowder= 50;
-
-calculate(milk,biscuit,cofeePowder)
-
-function calculate(a,b,c){
-    var result = a+b+c;
-return result;
-}
+// //variable reassign possible
+//  place="trichy";
+// console.log(place);
 
 
-var bread= 20;
-var bun =25;
-var milkPowder= 10;
-console.log(calculate(milk,biscuit,cofeePowder));
-console.log(calculate(bread,bun,milkPowder));
+// //Redclare not possible
+// //variable reassign possible
+// const num=3;
+// console.log(num);
 
-//for loop
-for(person=0;person<100;person++){
-console.log("go in:"+person);
-}
+// //data types: String,number,boolean,null,undefined,symbols,object:
+
+// //functions
+
+// var milk= 23;
+// var biscuit =20;
+// var cofeePowder= 50;
+
+// calculate(milk,biscuit,cofeePowder)
+
+// function calculate(a,b,c){
+//     var result = a+b+c;
+// return result;
+// }
 
 
+// var bread= 20;
+// var bun =25;
+// var milkPowder= 10;
+// console.log(calculate(milk,biscuit,cofeePowder));
+// console.log(calculate(bread,bun,milkPowder));
 
+// //for loop
+// for(person=0;person<100;person++){
+// console.log("go in:"+person);
+// }
+// clearInterval();
+
+// var a=30;
+// var b=40;
+
+// function add(a,b){
+//     var result=a+b;
+//     return result;
+// }
+
+// console.log(add(a,b));
+
+// var factor = "kamal";
+// var fplayer="dhoni";
+// var fmovie="Anbe shivam";
+
+// function fav(){
+//     console.log("Favourite Actor:"+factor);
+//     console.log("Favourite Player:"+fplayer);
+//     console.log("Favourite Movie:"+fmovie);
+// }
+// fav();
+
+// function area(length,breath){
+//     console.log("area is :"+ length*breath);
+// }
+// area(20,30);
+var a=30;
+var b=40;
+
+function add(a,b){
+        var result=a+b;
+        return result;
+    }
+    
+    console.log(add(a,b));
 
 
 
